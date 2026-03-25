1e54a805ef821bf2226d89a64e481c9fa1d62294
ivanu <ivan.udovychenko.25@kse.org.ua>
2026-03-25 14:38:43 +0200

    Added code to process json file.

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-import csv
-total_price = 0
-with open("data/products-1000.csv", "r", encoding="utf-8") as file:
-    reader = csv.reader(file)
-    prices = []
-    quant = []
-    next(file)
-    for row in reader:
-        if row[11] == "in_stock":
-            price = float(row[5])
-            stock = int(row[7])
-            total_price += price * stock
-            prices.append(price)
-            quant.append(stock)
+import json
 
-
-
-print(f"The total value is {total_price}, average is {sum(prices)/len(prices)}, maximum value is {max(prices)}, minimum value is {min(prices)}")
+with open("data/data.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+    total = 0
+    for item in data:
+        if item["state"] == "California":
+            total += 1
+    print(f" The amount of californians {total}, out of {len(data)}")

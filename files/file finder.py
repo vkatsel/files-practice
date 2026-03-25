aa9ffa9f33dff38dcf48fbaf2706752e579a8fc5
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-25 17:53:50 +0200

    Тепер ця програма робить копію того файлу, який в ввели під тою назвою, що ви ввели

--- files/file finder.py
+++ files/file finder.py
@@ -1,4 +1,8 @@
-f_name= input()
-with open(f_name,'r', encoding="utf-8") as file:
-    for line in file:
-        print(line)
+f_name, copy_f_name = map(str, input().split())
+try:
+    with open(copy_f_name, "w", encoding="utf-8") as filew:
+        with open(f_name,'r', encoding="utf-8") as filer:
+            for line in filer:
+                filew.write(line)
+except FileNotFoundError:
+    print("Йой, не можемо знайти цей файл! спробуйте ще раз")

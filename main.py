ce0388642ec79988df84ca946296340701e768f6
Vlad <sevcenkovlad407@gmail.com>
2026-03-25 14:54:26 +0200

    JSON file new

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ import json
 # print(max(prices))
 # print()
 # print(f"The total value is {price}")
+    # list_ca = [i for i in user_data if i['state'] == "Calisfornia"]
+    # print(f"The amount of Californians: {total}, out of {len(user_data)}")
 total = 0
-with open("data/data.json", "r", encoding="UTF-8") as file:
-    user_data = json.load(file)
-    list_ca = [i for i in user_data if i['state'] == "Calisfornia"]
-    print(list_ca)
-    print(f"The amount of Californians: {total}, out of {len(user_data)}")
+with open("data/data.json", "r", encoding="UTF-8") as file, \
+        open("data/data-ca.json", "w", encoding="UTF-8") as dest:
+        user_data = json.load(file)
+        list_oa = [i for i in user_data if i['state'] == "California"]
+        json.dump(list_oa, dest, indent=4)
+        print(list_oa)
 
 

1ad5444382b24119e89916941dbad083c344141b
ivanu <ivan.udovychenko.25@kse.org.ua>
2026-03-25 14:41:36 +0200

    Added code to process json file in a different way.

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import json
 
 with open("data/data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
-    total = 0
-    for item in data:
-        if item["state"] == "California":
-            total += 1
-    print(f" The amount of californians {total}, out of {len(data)}")
+    list_ca = [i for i in data if i["state"] == "California"]
+    print(f"The amount of California is {len(list_ca)}, out of {len(data)} ")

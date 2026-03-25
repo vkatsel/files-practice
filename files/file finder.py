0db1900b12830cb11871a18851f5f59095bda7fd
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-25 22:00:57 +0200

    Оформила код у пристойнішу функцію з рекурсією

--- files/file finder.py
+++ files/file finder.py
@@ -1,8 +1,12 @@
-f_name, copy_f_name = map(str, input().split())
-try:
-    with open(copy_f_name, "w", encoding="utf-8") as filew:
-        with open(f_name,'r', encoding="utf-8") as filer:
-            for line in filer:
-                filew.write(line)
-except FileNotFoundError:
-    print("Йой, не можемо знайти цей файл! спробуйте ще раз")
+
+def inp_file():
+    f_name, copy_f_name = map(str, input().split())
+    try:
+        with open(copy_f_name, "w", encoding="utf-8") as filew:
+            with open(f_name,'r', encoding="utf-8") as filer:
+                for line in filer:
+                    filew.write(line)
+    except FileNotFoundError:
+        print("Йой, не можемо знайти цей файл! спробуйте ще раз")
+        inp_file()
+inp_file()

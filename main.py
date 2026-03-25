2b1043e267905688f1f67e407b38c8fb71175346
o.semenov <oleksii.semenov.25@kse.org.ua>
2026-03-25 14:01:52 +0200

    add total_price counter and csv reader

---
+++ main.py
@@ -0,0 +1,13 @@
+import csv
+
+total_price = 0
+
+with open("data/products-1000.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(file)
+    for row in reader:
+        if row[11] == 'in_stock':
+            price = float(row[5])
+            stock = int(row[7])
+            total_price += price * stock
+print(f'The total value is {total_price}')

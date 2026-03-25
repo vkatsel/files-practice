5de1d5e004734d7a156217365f6d7d64accdbc80
ivanu <ivan.udovychenko.25@kse.org.ua>
2026-03-25 14:51:32 +0200

    Added code to process json and create a new json file.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 
-with open("data/data.json", "r", encoding="utf-8") as file:
+with open("data/data.json", "r", encoding="utf-8") as file, \
+        open("data/data-ca.json", "w", encoding="utf-8") as file2:
     data = json.load(file)
     list_ca = [i for i in data if i["state"] == "California"]
+    json.dump(list_ca, file2, indent=4 )
     print(f"The amount of California is {len(list_ca)}, out of {len(data)} ")

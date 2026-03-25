a0e7944c07f66cf863f5302c75340ad9eec28e8a
o.semenov <oleksii.semenov.25@kse.org.ua>
2026-03-25 14:25:57 +0200

    new branch for second task, avg_price and max_price

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import csv
 total_price = 0
 
 with open("data/products-1000.csv", "r", encoding="utf-8") as file:
-    reader = list(csv.DictReader(file))
+    reader = csv.reader(file)
+    prices = []
     for row in reader:
-        print(f"{row['Index']}{row['Name']}")
-    # next(file)
-    # for row in reader:
-    #     if row[11] == 'in_stock':
-    #         price = float(row[5])
-    #         stock = int(row[7])
-    #         total_price += price * stock
+        if row[11] == "in_stock":
+            prices.append(float(row[5]))
+    print(f"Average price is {round(sum(prices)/len(prices), 2)}, maximum price is {max(prices)}")
+
+
+

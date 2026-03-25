ceb1cbbd9699919af6635ebf18fffe8ea5c823f8
Maksym Telnyi <maksym.telnyi.25@kse.org.ua>
2026-03-25 14:08:37 +0200

    Реалізували у інший спосіб зчитування файлу через csv.DictReader(file)

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import csv
 
 total = 0
 with open("data/products-1000.csv", "r", encoding="UTF-8") as file:
-    reader = csv.reader(file)
+    reader = list(csv.DictReader(file))
     for row in reader:
-        if row[11] == "in_stock":
-            price=float(row[5])
-            stock = int(row[7])
-            total += price * stock
+        print(f"{row["Index"]} {row["Name"]}")
 
 
 print(f"The total value is {total}")

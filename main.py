d2cc1be2d77e720e551d9557db402ec96fdfb059
Maksym Larchenko <maksym.larchenko.25@kse.org.ua>
2026-03-25 14:43:03 +0200

    Попрацював із json та знайшов кількість каліфорнійців

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 # #reader - це спеціальний тип даних. Клас в csv, який нагадує json.
 # total = 0
 # with open("data/products-1000.csv", "r", encoding="UTF-8") as file:
@@ -19,23 +20,34 @@ import csv
 #     for row in reader:
 #         print(f"{row["Index"]} {row["Name"]}")
 # #створюємо список із словничків і row - це словник
-total_sum = 0
-counter = 0
+# total_sum = 0
+# counter = 0
+#
+# def maximum(dict):
+#     max = 0
+#     for el in dict:
+#         if int(el["Price"]) > max:
+#             max = int(el["Price"])
+#     return max
+#
+# with open("data/products-1000.csv", "r", encoding="UTF-8") as file:
+#     reader = list(csv.DictReader(file))
+#     for row in reader:
+#         if row["Availability"] == "in_stock":
+#             total_sum += float(row["Price"])
+#             counter += 1
+#     avr = total_sum/counter
+# print(f"The average is {avr} and the most expensive product costs {maximum(reader)}$")
+# total = 0
+# with open("data/data.json", "r", encoding = "utf-8") as file:
+#     user_data = json.load(file)
+#     # for el in user_data:
+#     #     if el["state"] == "California":
+#     #         total += 1
+#     list_ca = [i for i in user_data if i["state"] == 'California']
+#     print(list_ca)
+#     print(f"The amount of californians is {len(list_ca)}, out of {len(user_data)}")
 
-def maximum(dict):
-    max = 0
-    for el in dict:
-        if int(el["Price"]) > max:
-            max = int(el["Price"])
-    return max
 
-with open("data/products-1000.csv", "r", encoding="UTF-8") as file:
-    reader = list(csv.DictReader(file))
-    for row in reader:
-        if row["Availability"] == "in_stock":
-            total_sum += float(row["Price"])
-            counter += 1
-    avr = total_sum/counter
-print(f"The average is {avr} and the most expensive product costs {maximum(reader)} $")
 
 

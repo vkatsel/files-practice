6bd3f4793f8db0c4224175571f600f792854ae27
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-25 17:35:48 +0200

    Додала до першого пункту збереження помилок у окремому файлі

--- files/Task 1.py
+++ files/Task 1.py
@@ -1,7 +1,9 @@
-with open("logs.txt", "r", encoding="utf-8") as file:
-    cnt=0
-    for line in file:
-        if line[0]=='E':
-            print(line)
-            cnt+=1
-    print(f"Кількість критичних помилок: {cnt}")
+with open("errors.txt", "w", encoding="utf-8") as filew:
+    with open("logs.txt", "r", encoding="utf-8") as file:
+        cnt=0
+        for line in file:
+            if line[0]=='E':
+                filew.write(line)
+                cnt+=1
+        filew.write(f"Кількість критичних помилок: {cnt}")
+

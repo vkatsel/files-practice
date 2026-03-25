f3d5e0a03ff40bffe124e2ab47171d83be9bec2a
ivanu <ivan.udovychenko.25@kse.org.ua>
2026-03-25 14:31:58 +0200

    Added code to calculate average and maximum prices.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import csv
 total_price = 0
 with open("data/products-1000.csv", "r", encoding="utf-8") as file:
-    reader = list(csv.DictReader(file))
+    reader = csv.reader(file)
+    prices = []
+    quant = []
+    next(file)
     for row in reader:
-        print(f"{row["Index"]} {row["Name"]}")
+        if row[11] == "in_stock":
+            price = float(row[5])
+            stock = int(row[7])
+            total_price += price * stock
+            prices.append(price)
+            quant.append(stock)
+
+
+
+print(f"The total value is {total_price}, average is {sum(prices)/len(prices)}, maximum value is {max(prices)}, minimum value is {min(prices)}")

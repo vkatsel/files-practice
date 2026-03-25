ae4d09b27ff1a257d2859ff817db066c881cd12f
Vlad <sevcenkovlad407@gmail.com>
2026-03-25 14:29:42 +0200

    The max price and average price

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import csv
 
 with open("data/products-1000.csv", "r", encoding="UTF-8") as file:
     reader = list(csv.DictReader(file))
-    for row in reader:
-        print(f"{row['Index']} {row['Name']}")
+    total_amount = 0
+    sum = 0
+    for price in reader:
+        total_amount += 1
+        sum += int(price['Price'])
+    print(round(sum/total_amount, 2))
+prices = [int(row["Price"]) for row in reader]
+print(max(prices))
+print()
 
-print(f"The total value is {total}")
+
+# print(f"The total value is {price}")
 

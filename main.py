5a7d7d49efcd2092c785a69c74687231819a2fd8
Vlad <sevcenkovlad407@gmail.com>
2026-03-25 14:10:52 +0200

    Added DictReader

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import csv
 
 with open("data/products-1000.csv", "r", encoding="UTF-8") as file:
-    reader = csv.reader(file)
-    next(file)
+    reader = list(csv.DictReader(file))
     for row in reader:
-        if row[1] in reader:
-            price = float(row[5])
-            stock = int(row[7])
-            total = price*stock
+        print(f"{row['Index']} {row['Name']}")
 
 print(f"The total value is {total}")
+
